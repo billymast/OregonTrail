395955b1e7872fb66a558a2eb5e438ee2fa7ca0c
billymast <w-mast@onu.edu>
2023-04-12 13:13:00 -0400

    Billy Mast 4/12/23 1:12 PM

---
+++ app/src/main/java/com/example/oregontrail/River.java
@@ -0,0 +1,24 @@
+package com.example.oregontrail;
+
+// River Class: Object for Rivers which contain depth, width, river current swiftness, and floor type of river
+// Also contains functions involving outcomes that occur when crossing river under different circumstances
+// Author: Billy Mast
+
+public class River {
+
+    private int depth;
+    private int width;
+    private int swiftness;
+    private String riverFloorType;
+
+    public River(int depth, int width, int swiftness, String riverFloorType) {
+        this.depth = depth;
+        this.width = width;
+        this.swiftness = swiftness;
+        this.riverFloorType = riverFloorType;
+    }
+
+
+
+
+}
