6aeb21cf7e20db93ee8ce6285cc62ccae21a5019
ryan2nick <r-nichols.2@onu.edu>
2023-04-28 09:36:59 -0400

    (4/28/23) - 9:36 - Ryan

--- app/src/main/java/com/example/oregontrail/Hunting.java
+++ app/src/main/java/com/example/oregontrail/Hunting.java
@@ -4,90 +4,19 @@ import java.util.Scanner;
 
 public class Hunting {
 
-    int distance;
-    int location;
+    public void Hunting (){};
 
-    int[][] map = new int[9][3];
+    public boolean Hunt (Inventory inventory, int guess){
 
-    public void Hunting(){
-        distance = (int) Math.random() * 3;
-        location = (int) Math.random() * 10;
-        map[location][distance] = 1;
-    }
+        int shots = inventory.getInventoryValue("Shots");
 
-    public String distance () {
-        int Distance = distance;
-        switch (Distance){
-            case 0:
-                return "The distance is 10-20 yards";
-            case 1:
-                return "The distance is 30-40 yards";
-            case 2:
-                return "The distance is 50-60 yards";
-            default: return "There is no prey nearby";
-        }
-    }
+        int location = (int) (Math.random() * 5) + 1;
 
-    public boolean takeShot (Inventory inventory, String type, int guess){
-        double value = Math.random();
-        switch (type) {
-            case "Riffle":
-                if (inventory.getInventoryValue("Riffle") != 0){
-                    if (inventory.getInventoryValue("Shots") != 0) {
-                        if (guess == location) {
-                            switch (distance) {
-                                case 0:
-                                    if (value < 0.50) {
-                                        return true;
-                                    }
-                                    break;
-                                case 1:
-                                    if (value < 0.25) {
-                                        return true;
-                                    }
-                                    break;
-                                case 2:
-                                    if (value < 0.10) {
-                                        return true;
-                                    }
-                                    break;
-                                default:
-                                    return false;
-                            }
-                        }
-                    }
-                }
-                break;
-            case "Shotgun":
-                if (inventory.getInventoryValue("Shotgun") != 0){
-                    if (inventory.getInventoryValue("Shots") != 0){
-                        switch (distance){
-                            case 0:
-                                if (guess == location){
-                                    if(value < 0.50){
-                                        return true;
-                                    }
-                                }
-                                break;
-                            case 1:
-                                if (guess == location){
-                                    if (value < 0.25){
-                                        return true;
-                                    }
-                                }
-                                break;
-                            case 2:
-                                if (guess == location){
-                                    if (value < 0.10){
-                                        return true;
-                                    }
-                                }
-                            default: return false;
-                        }
-                    }
-                }
-
-
+        if (shots != 0){
+            if (guess == location) {
+                inventory.removeInventory("Shots", 1);
+                return true;
+            }else {return false;}
         }
         return false;
     }
